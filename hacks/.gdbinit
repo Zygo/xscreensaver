41fae2ad67bc37e31c4d967bae81e4f3f50fa55a
Zygo Blaxell <zblaxell@hungrycats.org>
2009-03-02 00:42:35 -0500

    ftp://ftp.sunet.se/pub/vendor/sco/skunkware/osr5/x11/savers/xscreensaver/xscreensaver-3.15-dist.tar.gz -rw-r--r-- 1 zblaxell zblaxell 2851086 Feb 27 16:43 xscreensaver-3.15-dist.tar.gz 9154ee3f0f70ba8e83ef29c136c07f453d505574  xscreensaver-3.15-dist.tar.gz

--- hacks/.gdbinit
+++
@@ -1,4 +0,0 @@
-set args -geom =600x480+0+0 -sync
-b screenhack_ehandler
-b exit
-b abort
--- utils/version.h
+++
@@ -1,2 +0,0 @@
-static const char screensaver_id[] =
-	"@(#)xscreensaver 3.12 (10-May-99), by Jamie Zawinski (jwz@jwz.org)";
