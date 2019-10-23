e9c6167b761fca9386e22d443c44819a94965b58
mind_combatant <admin@mindcombatant.com>
2019-10-23 01:58:18 -0700

    Updated source .tar.gz from jwz including things he seems to have forgotten in the initial 5.43, but didn't rename or move the file

--- utils/version.h
+++ utils/version.h
@@ -1,2 +1,2 @@
 static const char screensaver_id[] =
-	"@(#)xscreensaver 5.43 (01-Jul-2019), by Jamie Zawinski (jwz@jwz.org)";
+	"@(#)xscreensaver 5.43 (09-Jul-2019), by Jamie Zawinski (jwz@jwz.org)";
