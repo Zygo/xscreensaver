4b94cbf538c684e584b85f604956f00f0b0fb904
gesh <gesh@gesh.uni.cx>
2026-01-18 17:14:07 +0200

    OSX: Update to Sparkle 2.8

--- OSX/Sparkle.framework/Versions/B/Headers/SPUStandardUpdaterController.h
+++ OSX/Sparkle.framework/Versions/B/Headers/SPUStandardUpdaterController.h
@@ -28,15 +28,17 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  A controller class that instantiates a `SPUUpdater` and allows binding UI to its updater settings.
  
- This class can be instantiated in a nib or created programatically using `-initWithUpdaterDelegate:userDriverDelegate:` or `-initWithStartingUpdater:updaterDelegate:userDriverDelegate:`.
+ This class can be instantiated in a nib or created programmatically using `-initWithUpdaterDelegate:userDriverDelegate:` or `-initWithStartingUpdater:updaterDelegate:userDriverDelegate:`.
  
  The controller's updater targets the application's main bundle and uses Sparkle's standard user interface.
- Typically, this class is used by sticking it as a custom NSObject subclass in an Interface Builder nib (probably in MainMenu) but it works well programatically too.
+ Typically, this class is used by sticking it as a custom NSObject subclass in an Interface Builder nib (probably in MainMenu) but it works well programmatically too.
  
  The controller creates an `SPUUpdater` instance using a `SPUStandardUserDriver` and allows hooking up the check for updates action and handling menu item validation.
  It also allows hooking up the updater's and user driver's delegates.
  
  If you need more control over what bundle you want to update, or you want to provide a custom user interface (via `SPUUserDriver`), please use `SPUUpdater` directly instead.
+ 
+ This class must be used on the main thread.
   */
 SU_EXPORT @interface SPUStandardUpdaterController : NSObject
 {
@@ -71,7 +73,7 @@ SU_EXPORT @interface SPUStandardUpdaterController : NSObject
  
  You cannot call this initializer directly. You must instantiate a `SPUStandardUpdaterController` inside of a nib (typically the MainMenu nib) to use it.
  
- To create a `SPUStandardUpdaterController` programatically, use `-initWithUpdaterDelegate:userDriverDelegate:` or `-initWithStartingUpdater:updaterDelegate:userDriverDelegate:` instead.
+ To create a `SPUStandardUpdaterController` programmatically, use `-initWithUpdaterDelegate:userDriverDelegate:` or `-initWithStartingUpdater:updaterDelegate:userDriverDelegate:` instead.
  */
 - (instancetype)init NS_UNAVAILABLE;
 
@@ -79,6 +81,8 @@ SU_EXPORT @interface SPUStandardUpdaterController : NSObject
  Create a new `SPUStandardUpdaterController` programmatically.
  
  The updater is started automatically. See `-startUpdater`  for more information.
+ 
+ Note the `updaterDelegate` and `userDriverDelegate` are weakly referenced, so you are responsible for keeping them alive.
  */
 - (instancetype)initWithUpdaterDelegate:(nullable id<SPUUpdaterDelegate>)updaterDelegate userDriverDelegate:(nullable id<SPUStandardUserDriverDelegate>)userDriverDelegate;
 
@@ -87,6 +91,8 @@ SU_EXPORT @interface SPUStandardUpdaterController : NSObject
  
  You can specify whether or not you want to start the updater immediately.
  If you do not start the updater, you must invoke `-startUpdater` at a later time to start it.
+ 
+ Note the `updaterDelegate` and `userDriverDelegate` are weakly referenced, so you are responsible for keeping them alive.
  */
 - (instancetype)initWithStartingUpdater:(BOOL)startUpdater updaterDelegate:(nullable id<SPUUpdaterDelegate>)updaterDelegate userDriverDelegate:(nullable id<SPUStandardUserDriverDelegate>)userDriverDelegate;
 
